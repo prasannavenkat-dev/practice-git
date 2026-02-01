1d7f9e9660d9b381ffbc6a62b97677244356bd44
prasannavenkat-dev <74189369+prasannavenkat-dev@users.noreply.github.com>
2026-02-01 17:44:22 +0530

    bella ciao

--- sample.js
+++ sample.js
@@ -1,7 +1,2 @@
 // This is sample js file for testing purpose 
-let js = {
-    a:"Hello There!",
-    c:"This is not testing phase, Dev phase",
-    e:"Dont commit any changes",
-    f:"temp changes i made"
-}
+let heist = ["money","heist","bella","ciao"]
